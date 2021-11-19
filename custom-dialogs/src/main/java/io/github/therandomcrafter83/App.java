2b2213d1070a86bb4eec7cdcb568ded129b40f4e
TheRandomCrafter83 <williams.carl.83@gmail.com>
2021-11-19 12:14:19 -0500

    added a key file

---
+++ custom-dialogs/src/main/java/io/github/therandomcrafter83/App.java
@@ -0,0 +1,13 @@
+package io.github.therandomcrafter83;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
